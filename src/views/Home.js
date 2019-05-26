0a126bd9a7c33ac953ff41403595fb6f4f4f0d2b
Fredy Blandon <fsblandon@gmail.com>
2019-05-25 20:53:06 -0500

    movies with fetch

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,9 +2,13 @@ import React from 'react';
 import moviesData from '../data/movies.json'
 import MovieCard from '../components/MovieCard'
 
+
+const API_KEY = '06ced2305eaef2fd6deb916ee2da825f';
+//let dataApi = [];
+
 class Home extends React.Component {
   state = {
-   ...moviesData,
+   ...moviesData
   }
 
   deleteMovie = (movieId) => {
@@ -16,16 +20,23 @@ class Home extends React.Component {
     })
   }
 
+  componentDidMount() {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((res) => res.json())
+    .then((data) => this.setState({movies : data.results.slice(0,6)}));
+  }
 
-    render() {
-        const { movies } = this.state
-        return <div>
-        <h1 className='main-title'>Movie App</h1>
+  render() {
+      const { movies } = this.state;
+      console.log(movies);
+      return <div>
+      <h1 className='main-title'>Movie App</h1>
         <div className='content'>
           {movies.map((movie) => <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />)}
         </div>
-      </div>
-    }
+    </div>
+  }
+
 }
 
 export default Home
